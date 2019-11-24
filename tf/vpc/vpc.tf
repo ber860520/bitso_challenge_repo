e4f54b75a3d30904f0a415b342c6f95ce736fd2f
Ber <ber860520@hotmail.com>
2019-11-24 11:53:07 -0600

    Adding IGW and initial routing table

--- tf/vpc/vpc.tf
+++ tf/vpc/vpc.tf
@@ -10,11 +10,49 @@ resource "aws_vpc" "prod-vpc" {
     }
 }
 
+
+#Internet GW 
+resource "aws_internet_gateway" "gateway_bitso" {
+	vpc_id = aws_vpc.prod-vpc.id
+}
+
+
+/*
+Public resources
+*/
+
 resource "aws_subnet" "pub_app_subnet"{
+	vpc_id = aws_vpc.prod-vpc.id
+	cidr_block = "10.0.1.0/24"
+	map_public_ip_on_launch = "true"
+	tags= {
+		    Name = "pub_app_subnet"
+	}
+}
+
+resource "aws_route_table" "pub_route_table"{
+	vpc_id = aws_vpc.prod-vpc.id
+	route{
+		cidr_block = "0.0.0./0"
+		gateway_id = aws_internet_gateway.gateway_bitso.id
+	}
+	tags ={
+		Name = "Subnet with public access"
+	}
+}
+
+
+/*
+Private resources
+*/
+
+resource "aws_subnet" "priv_db_subnet"{
 vpc_id = aws_vpc.prod-vpc.id
-cidr_block = "10.0.1.0/24"
+cidr_block = "10.0.2.0/24"
 map_public_ip_on_launch = "true"
 tags= {
-    Name = "pub_app_subnet"
+    Name = "priv_db_subnet"
 }
 }
+
+
