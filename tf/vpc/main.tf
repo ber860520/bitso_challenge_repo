325db5ea34d275e7f10a38edb3ba0cf6492d61a3
Ber <ber860520@hotmail.com>
2019-11-23 23:22:24 -0600

    Dev branch with initial VPC conf and gitignore

--- tf/vpc/main.tf
+++ tf/vpc/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                  = "us-east-1"
   shared_credentials_file = "/home/bmartinez/.aws/credentials"
-  profile                 = "terraform"
+  profile                 = "terraform-user"
 }
